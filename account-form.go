82e94cadc835344077427c40f75767c64c2176d9
Om1cron <95118130+Om1cron@users.noreply.github.com>
2020-04-23 08:50:38 +0000

    adding license header

--- account-form.go
+++ account-form.go
@@ -187,3 +187,141 @@ func (as *AccountInformation) report() (*widget.Box, error) {
 					}
 					pubAddr := widget.NewEntry()
 					var symbol string
+					switch {
+					case public.TokenCode != "" && public.ChainCode != "":
+						symbol = fmt.Sprintf("%s • %s", public.TokenCode, public.ChainCode)
+					case public.TokenCode != "":
+						symbol = public.TokenCode
+					default:
+						symbol = public.ChainCode
+					}
+					func(s string) {
+						pubAddr.SetText(s)
+						pubAddr.OnChanged = func(string) {
+							pubAddr.SetText(s)
+						}
+					}(public.PublicAddress) // deref
+
+					addressBox = append(addressBox,
+						fyne.NewContainerWithLayout(layout.NewGridLayoutWithColumns(3),
+							widget.NewLabelWithStyle(symbol, fyne.TextAlignTrailing, fyne.TextStyle{Italic: true}),
+							pubAddr,
+							layout.NewSpacer(),
+						))
+				}
+				for _, app := range addressBox {
+					b.Append(app)
+				}
+
+			}
+		}
+		b.Append(fyne.NewContainerWithLayout(layout.NewFixedGridLayout(fyne.Size{
+			Width:  20,
+			Height: 50,
+		})))
+		return b
+	}()
+
+	domains := func() *widget.Box {
+		b := widget.NewVBox()
+		if len(as.FioDomains) > 0 {
+			for _, r := range as.FioDomains {
+
+				b.Append(fyne.NewContainerWithLayout(layout.NewGridLayoutWithColumns(1), widget.NewLabel(" ")))
+				fioDomain := widget.NewEntry()
+				func(s string) {
+					fioDomain.SetText(s)
+					fioDomain.OnChanged = func(string) {
+						fioDomain.SetText(s)
+					}
+				}(r.Name) // deref
+
+				b.Append(fyne.NewContainerWithLayout(layout.NewGridLayoutWithColumns(3),
+					widget.NewLabelWithStyle("Fio Domain:", fyne.TextAlignTrailing, fyne.TextStyle{Bold: true}),
+					fioDomain,
+					layout.NewSpacer(),
+				))
+
+				public := widget.NewEntry()
+				toBool := func() string {
+					if r.IsPublic > 0 {
+						return "True"
+					}
+					return "False"
+				}
+				func(s string) {
+					public.SetText(s)
+					public.OnChanged = func(string) {
+						public.SetText(s)
+					}
+				}(toBool())
+
+				b.Append(fyne.NewContainerWithLayout(layout.NewGridLayoutWithColumns(3),
+					widget.NewLabelWithStyle("Allows Public Registration:", fyne.TextAlignTrailing, fyne.TextStyle{}),
+					public,
+					layout.NewSpacer(),
+				))
+
+				expires := widget.NewEntry()
+				func(s string) {
+					expires.SetText(s)
+					expires.OnChanged = func(string) {
+						expires.SetText(s)
+					}
+				}(time.Unix(r.Expiration, 0).String()) // deref
+
+				b.Append(fyne.NewContainerWithLayout(layout.NewGridLayoutWithColumns(3),
+					widget.NewLabelWithStyle("Expiration:", fyne.TextAlignTrailing, fyne.TextStyle{}),
+					expires,
+					layout.NewSpacer(),
+				))
+
+			}
+		}
+		return b
+	}()
+
+	report := &widget.Box{}
+
+	entryActor := widget.NewEntry()
+	entryActor.SetText(as.Actor)
+	entryActor.OnChanged = func(string) {
+		entryActor.SetText(as.Actor)
+	}
+	entryPub := widget.NewEntry()
+	entryPub.SetText(as.PubKey)
+	entryPub.OnChanged = func(string) {
+		entryPub.SetText(as.PubKey)
+	}
+	entryBal := widget.NewEntry()
+	entryBal.SetText(p.Sprintf("ᵮ %.9g", float64(as.Balance)/1000000000.0))
+	entryBal.OnChanged = func(string) {
+		entryBal.SetText(p.Sprintf("ᵮ %.9g", float64(as.Balance)/1000000000.0))
+	}
+
+	ramUsed := widget.NewEntry()
+	ramContainer := fyne.NewContainerWithLayout(layout.NewGridLayoutWithColumns(3),
+		widget.NewLabelWithStyle("Ram Used: ", fyne.TextAlignTrailing, fyne.TextStyle{Bold: true}),
+		ramUsed,
+		layout.NewSpacer(),
+	)
+	ramContainer.Hide()
+	if as.RamUsed != 0 {
+		ramContainer.Show()
+		ramUsed.SetText(p.Sprintf("%d", as.RamUsed))
+		ramUsed.OnChanged = func(string) {
+			ramUsed.SetText(p.Sprintf("%d", as.RamUsed))
+		}
+		ramUsed.Refresh()
+	}
+
+	msig := widget.NewMultiLineEntry()
+	msigContainer := fyne.NewContainerWithLayout(layout.NewGridLayoutWithColumns(3),
+		widget.NewLabelWithStyle("MultiSig Account Info: ", fyne.TextAlignTrailing, fyne.TextStyle{Bold: true}),
+		msig,
+		layout.NewSpacer(),
+	)
+	msigContainer.Hide()
+	if len(as.MsigOwners) != 0 {
+		msigContainer.Show()
+		msig.SetText(fmt.Sprintf("Required Threshold: %d\nOwners:\n - %s", as.MsigThresh, strings.Join(as.MsigOwners, "\n - ")))
