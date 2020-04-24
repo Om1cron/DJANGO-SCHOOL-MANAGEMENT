858ff9d4f58345216f4cff7104f589ef8f85715f
Om1cron <95118130+Om1cron@users.noreply.github.com>
2020-04-24 11:18:40 +0000

    ASoC: wm8960: Fix wrong value references for boolean kctl

--- account-info.go
+++ account-info.go
@@ -115,3 +115,136 @@ func AccountSearch(searchFor string, value string) (as *AccountInformation, err 
 	case "Actor/Account":
 		return as, as.searchForActor(value)
 	case "Public Key":
+		return as, as.searchForPub(value)
+	case "Private Key":
+		return as, as.searchForPriv(value)
+	case "Fio Address":
+		return as, as.searchForAddr(value)
+	case "Fio Domain":
+		return as, as.searchForDom(value)
+	}
+	return nil, nil
+}
+
+type aMap struct {
+	Clientkey string `json:"clientkey"`
+}
+
+func (as *AccountInformation) searchForActor(s string) error {
+	if s == "eosio" || strings.HasPrefix(s, "eosio.") || strings.HasPrefix(s, "fio.") {
+		resp, err := as.api.GetFioAccount(s)
+		if err != nil {
+			return err
+		}
+		as.PubKey = "n/a"
+		as.Actor = s
+		if len(resp.Permissions) > 0 {
+			if len(resp.Permissions[0].RequiredAuth.Keys) == 1 {
+				as.PubKey = resp.Permissions[0].RequiredAuth.Keys[0].PublicKey.String()
+			}
+			for _, p := range resp.Permissions {
+				if len(p.RequiredAuth.Accounts) > 0 {
+					for _, a := range p.RequiredAuth.Accounts {
+						as.MsigOwners = append(as.MsigOwners, string(a.Permission.Actor))
+					}
+				}
+			}
+		}
+		if as.PubKey != "n/a" {
+			as.PubKey = "Warning, not msig! - " + as.PubKey
+		}
+		return nil
+	}
+	name, err := eos.StringToName(s)
+	if err != nil {
+		return err
+	}
+	resp, err := as.api.GetTableRows(eos.GetTableRowsRequest{
+		Code:       "fio.address",
+		Scope:      "fio.address",
+		Table:      "accountmap",
+		LowerBound: fmt.Sprintf("%d", name),
+		UpperBound: fmt.Sprintf("%d", name),
+		Limit:      math.MaxInt32,
+		KeyType:    "i64",
+		Index:      "1",
+		JSON:       true,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	found := make([]aMap, 0)
+	err = json.Unmarshal(resp.Rows, &found)
+	if err != nil {
+		return err
+	}
+	if len(found) == 0 {
+		return errors.New("no matching account found in fio.address accountmap table")
+	}
+	as.Actor = s
+	as.PubKey = found[0].Clientkey
+	return as.searchForPub(as.PubKey)
+}
+
+func (as *AccountInformation) searchForPub(s string) error {
+	names, found, err := as.api.GetFioNames(s)
+	if err != nil {
+		return err
+	}
+	as.PubKey = s
+	a, err := fio.ActorFromPub(s)
+	if err != nil {
+		return err
+	}
+	assets, err := as.api.GetCurrencyBalance(a, "FIO", "fio.token")
+	if err != nil {
+		return err
+	}
+	if len(assets) > 0 {
+		as.Balance = int64(assets[0].Amount)
+	}
+	as.Actor = string(a)
+	if found {
+		for _, n := range names.FioAddresses {
+			as.fioNames = appendUniq(as.fioNames, n.FioAddress)
+		}
+		for _, n := range names.FioDomains {
+			as.fioDomains = appendUniq(as.fioDomains, n.FioDomain)
+		}
+	}
+	as.getFioNames()
+	as.getFioDomains()
+	as.getExtra()
+	return nil
+}
+
+func (as *AccountInformation) searchForPriv(s string) error {
+	a, err := fio.NewAccountFromWif(s)
+	if err != nil {
+		return err
+	}
+	as.PrivKey = s
+	as.PubKey = a.PubKey
+	return as.searchForPub(a.PubKey)
+}
+
+func (as *AccountInformation) searchForAddr(s string) error {
+	pubAddr, found, err := as.api.PubAddressLookup(fio.Address(s), "FIO", "FIO")
+	if err != nil {
+		return err
+	}
+	if !found {
+		return errors.New("did not find any FIO public keys for that address")
+	}
+	as.fioNames = appendUniq(as.fioNames, s)
+	as.PubKey = pubAddr.PublicAddress
+	return as.searchForPub(pubAddr.PublicAddress)
+}
+
+func (as *AccountInformation) getFioNames() {
+	const limit = 20
+	n, err := eos.StringToName(as.Actor)
+	if err != nil {
+		errs.ErrChan <- err.Error()
+		return
+	}
