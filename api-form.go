e9119cf84f9ee3e228aa9361358b6ffba7374c59
Om1cron <95118130+Om1cron@users.noreply.github.com>
2020-06-29 09:58:24 +0000

    Update talks.md

--- api-form.go
+++ api-form.go
@@ -55,3 +55,139 @@ func NewApiRequestTab(container chan fyne.Container) {
 			if err != nil {
 				outputEntry.SetText(err.Error())
 				errs.ErrChan <- err.Error()
+				return
+			}
+			statusLabel.SetText(fmt.Sprintf("POST %s -- %s", Uri+apiEndPointActive, resp.Status))
+			if resp.Body != nil {
+				defer func() {
+					resp.Body.Close()
+					outputEntry.Refresh()
+				}()
+				body, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					outputEntry.SetText(err.Error())
+					errs.ErrChan <- err.Error()
+					return
+				}
+				if len(body) > 131072 {
+					outputEntry.SetText("Response body is too big to show")
+					errs.ErrChan <- "Response body is too big to show"
+					return
+				}
+				j, err := json.MarshalIndent(json.RawMessage(body), "", "  ")
+				if err != nil {
+					outputEntry.SetText(err.Error())
+					errs.ErrChan <- err.Error()
+					return
+				}
+				txt := string(j)
+				func(s string) {
+					outputEntry.OnChanged = func(string) {
+						outputEntry.SetText(s)
+					}
+				}(txt) // deref
+				outputEntry.SetText(txt)
+			}
+		}()
+		for {
+			select {
+			case <-ctx.Done():
+				outputEntry.SetText("Request timed out.")
+				return
+			case <-done:
+				return
+			}
+		}
+	}) // NewButtonWithIcon
+
+	apiRadio := &widget.Radio{}
+	epbFilter := widget.NewEntry()
+	epbFilter.SetPlaceHolder("Filter Endpoints")
+
+	apiUpdate := func(ep string) {
+		apiEndPointActive = ep
+		submit.SetText(ep)
+		submit.Refresh()
+		inputEntry.SetText(DefaultJsonFor(ep))
+		inputEntry.Refresh()
+	}
+	apiRadio = widget.NewRadio(apiList.Apis, apiUpdate)
+
+	copyToClip := &widget.Button{}
+	clipped := func() {
+		go func() {
+			clip := Win.Clipboard()
+			curl := fmt.Sprintf(`curl -s -XPOST %s -d %s`, shellescape.Quote(Uri+apiEndPointActive), shellescape.Quote(inputEntry.Text))
+			clip.SetContent(curl)
+			copyToClip.Text = "Copied!"
+			if curl != clip.Content() {
+				clip.SetContent("Failed to Copy!")
+			}
+			copyToClip.Refresh()
+			time.Sleep(2 * time.Second)
+			copyToClip.Text = "Copy as curl"
+			copyToClip.Refresh()
+		}()
+	}
+	copyToClip = widget.NewButtonWithIcon("Copy as curl", theme.ContentCopyIcon(), clipped)
+
+	hideSigned := &widget.Check{}
+	hide := func(b bool) {
+		apiRadio.Options = nil
+		apiRadio.Refresh()
+		newList := make([]string, 0)
+		for i := range apiList.Apis {
+			switch true {
+			case hideSigned.Checked && isSigned(apiList.Apis[i]):
+				break
+			case epbFilter.Text == "":
+				newList = append(newList, apiList.Apis[i])
+			case strings.Contains(apiList.Apis[i], epbFilter.Text):
+				newList = append(newList, apiList.Apis[i])
+			}
+		}
+		apiRadio.Options = newList
+		apiRadio.Refresh()
+	}
+	hideSigned = widget.NewCheck("Hide Signed", hide)
+
+	submit.SetText(apiEndPointActive)
+	submit.Refresh()
+	inputTab = widget.NewTabItem("Request",
+		fyne.NewContainerWithLayout(layout.NewHBoxLayout(),
+			fyne.NewContainerWithLayout(layout.NewFixedGridLayout(fyne.Size{Width: apiRadio.MinSize().Width + 10, Height: int(math.Round(float64(H) * .63))}),
+				widget.NewVBox(
+					widget.NewHBox(hideSigned, epbFilter),
+					fyne.NewContainerWithLayout(layout.NewFixedGridLayout(fyne.Size{Width: apiRadio.MinSize().Width + 10, Height: int(math.Round(float64(H) * .63))}),
+						widget.NewGroupWithScroller("Endpoint", apiRadio),
+					),
+				),
+			),
+			fyne.NewContainerWithLayout(layout.NewFixedGridLayout(fyne.Size{Width: 30, Height: H / 3})),
+			fyne.NewContainerWithLayout(layout.NewFixedGridLayout(fyne.Size{Width: W - apiRadio.MinSize().Width - 360, Height: int(math.Round(float64(H) * .63))}),
+				widget.NewScrollContainer(widget.NewVBox(
+					widget.NewHBox(
+						layout.NewSpacer(),
+						submit,
+						copyToClip,
+						layout.NewSpacer(),
+					),
+					widget.NewLabelWithStyle("Request JSON:", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+					inputEntry,
+					layout.NewSpacer(),
+				))),
+		),
+	)
+	outputTab = widget.NewTabItem("Response",
+		widget.NewScrollContainer(widget.NewVBox(
+			widget.NewHBox(widget.NewLabel("Resend:"), submit, layout.NewSpacer(), statusLabel, layout.NewSpacer(), layout.NewSpacer()),
+			outputEntry,
+			fyne.NewContainerWithLayout(layout.NewFixedGridLayout(fyne.Size{
+				Width:  100,
+				Height: 100,
+			})),
+		)),
+	)
+	epbFilter.OnChanged = func(string) {
+		hide(hideSigned.Checked)
+	}
