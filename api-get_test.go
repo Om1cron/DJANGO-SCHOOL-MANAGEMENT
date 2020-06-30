9e0bddf3767f8fbc73c60690c9b0ea42d4996a07
Om1cron <95118130+Om1cron@users.noreply.github.com>
2020-06-30 22:49:38 +0000

    quota: provide interface for readding allocated space into reserved space

--- api-get_test.go
+++ api-get_test.go
@@ -3,4 +3,12 @@ package cryptonym
 //func TestFetchApis(t *testing.T) {
 //	list := SupportedApis{}
 //	api, _, err := fio.NewConnection(nil, "http://127.0.0.1:8888")
-//
+//	if err != nil {
+//		t.Error(err)
+//	}
+//	err = list.Update(api)
+//	if err != nil {
+//		t.Error(err)
+//	}
+//	if len(list.Apis) == 0 {
+//		t.Error("supporte
