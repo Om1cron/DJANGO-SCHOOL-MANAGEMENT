90d458de253fa584a859af5d6ce52a7b97eab8b6
Om1cron <95118130+Om1cron@users.noreply.github.com>
2022-03-12 07:23:23 +0000

    Updated the readme file

--- log_darwin.go
+++ log_darwin.go
@@ -12,4 +12,6 @@ func startErrLog() {
 	d, e := os.UserConfigDir()
 	if e != nil {
 		log.Println(e)
-		retur
+		return
+	}
+	errLog, e := os.OpenFile(fmt.Sprintf("%s%c%s%cerror.log", d, os.PathSeparator, settingsDir, os.PathSeparator), os.O_CREATE|os.O_
