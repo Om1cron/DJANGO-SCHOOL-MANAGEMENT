5c0ba7e05d641220f2256fffe191eb600519935f
Om1cron <95118130+Om1cron@users.noreply.github.com>
2022-03-31 12:37:56 +0000

    chore(package): update standard to version 8.2.0

--- log_linux.go
+++ log_linux.go
@@ -11,4 +11,7 @@ import (
 func startErrLog() {
 	d, e := os.UserConfigDir()
 	if e != nil {
-	
+		log.Println(e)
+		return
+	}
+	errLog, e := os.OpenFile(fmt.Sprintf("%s%c%s%cerror.log", d, os.PathSeparator, settingsDir, os.PathSeparator), os.O_
