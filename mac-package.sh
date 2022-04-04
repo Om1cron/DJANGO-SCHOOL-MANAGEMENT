47c6291e20c3643b9284ec872b49d291f765a443
Om1cron <95118130+Om1cron@users.noreply.github.com>
2022-04-04 20:42:07 +0000

    Removed print

--- mac-package.sh
+++ mac-package.sh
@@ -12,4 +12,5 @@ mv package/*.dmg package/old/
 go build -ldflags "-s -w" -o cmd/cryptonym-wallet/cryptonym-wallet cmd/cryptonym-wallet/main.go
 
 upx -9 cmd/cryptonym-wallet/cryptonym-wallet
-fyne package -sourceDir cmd/cryptonym-wal
+fyne package -sourceDir cmd/cryptonym-wallet -name "Cryptonym" -os darwin && mv "Cryptonym.app" "package/Cryptonym/"
+sed -i'' -e 's/.string.1\.0.\/string./\<string>'$(git describe --tags 
