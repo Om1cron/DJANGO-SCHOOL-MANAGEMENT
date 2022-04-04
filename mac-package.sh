68a6971ae6394e0428d29d2149f8a145e08ffa0a
Om1cron <95118130+Om1cron@users.noreply.github.com>
2022-04-04 18:23:08 +0000

    Update checklist.md

--- mac-package.sh
+++ mac-package.sh
@@ -3,4 +3,10 @@
 # builds a macos package (.app) and places it inside a compressed .dmg
 
 export CGO_CFLAGS="-mmacosx-version-min=10.14"
-export CGO_LDFLAGS="-mmacosx-version-
+export CGO_LDFLAGS="-mmacosx-version-min=10.14"
+
+mkdir -p "package/Cryptonym"
+mkdir -p package/old
+mv package/*.dmg package/old/
+
+go build -ldflags "-s -w" -o cmd/cryptonym-wallet/c
