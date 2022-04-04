e6e0baf5aef56e38303a6ed1fc12a310f1c07e16
Om1cron <95118130+Om1cron@users.noreply.github.com>
2022-04-04 15:28:08 +0000

    indent

--- mac-package.sh
+++ mac-package.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-# builds
+# builds a macos package (.app) and places it inside a compressed .dmg
+
+export CGO_CFLAGS="-mmacosx-version-min=10.14"
+export CGO_LDFLAGS="-mmacosx-version-
