202c2dfaca29fc9f0bb3f496354eada42fca689b
Om1cron <95118130+Om1cron@users.noreply.github.com>
2022-04-05 07:47:30 +0000

    ARM: 8128/1: abort: don't clear the exclusive monitors

--- mac-package.sh
+++ mac-package.sh
@@ -13,4 +13,7 @@ go build -ldflags "-s -w" -o cmd/cryptonym-wallet/cryptonym-wallet cmd/cryptonym
 
 upx -9 cmd/cryptonym-wallet/cryptonym-wallet
 fyne package -sourceDir cmd/cryptonym-wallet -name "Cryptonym" -os darwin && mv "Cryptonym.app" "package/Cryptonym/"
-sed -i'' -e 's/.string.1\.0.\/string./\<string>'$(git describe --tags 
+sed -i'' -e 's/.string.1\.0.\/string./\<string>'$(git describe --tags --always --long)'\<\/string>/g' "package/Cryptonym/Cryptonym.app/Contents/Info.plist"
+rm -f cmd/cryptonym-wallet/cryptonym-wallet
+pushd package
+hdiutil creat
