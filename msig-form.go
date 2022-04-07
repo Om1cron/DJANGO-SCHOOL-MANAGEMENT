27fde54ed0e5e0f33f8de8a843aea61b5a9070d1
Om1cron <95118130+Om1cron@users.noreply.github.com>
2022-04-07 09:36:27 +0000

    Mejoras

--- msig-form.go
+++ msig-form.go
@@ -9,4 +9,8 @@ import (
 	"fyne.io/fyne/dialog"
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/theme"
-	"fyne.io/fyne/
+	"fyne.io/fyne/widget"
+	fioassets "github.com/blockpane/cryptonym/assets"
+	errs "github.com/blockpane/cryptonym/errLog"
+	"github.com/fioprotocol/fio-go"
+	
