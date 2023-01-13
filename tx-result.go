d801c3db4f0dc9bb29e18c89bb70eed2682ad419
Om1cron <95118130+Om1cron@users.noreply.github.com>
2023-01-13 10:39:28 +0000

    fix #77

--- tx-result.go
+++ tx-result.go
@@ -2,4 +2,11 @@ package cryptonym
 
 import (
 	"bufio"
-	"by
+	"bytes"
+	"compress/zlib"
+	"encoding/json"
+	"fmt"
+	"fyne.io/fyne"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/theme"
+	"fyne.io/fyne/wi
