87e0e6958c477a7cf269fa5e7861dbd6cb2faac6
Om1cron <95118130+Om1cron@users.noreply.github.com>
2023-06-04 06:50:04 +0000

    hahaha

---
+++ vote.go
@@ -0,0 +1,16 @@
+
+package cryptonym
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"fyne.io/fyne"
+	"fyne.io/fyne/canvas"
+	"fyne.io/fyne/dialog"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/theme"
+	"fyne.io/fyne/widget"
+	fioassets "github.com/blockpane/cryptonym/assets"
+	errs "github.com/blockpane/cryptonym/errLog"
+	"github.com/fioprotocol/fio-go"
